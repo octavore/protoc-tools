e47022f075254afab622c6f349e07391e55898ea
tp <tp@octavore.com>
2024-02-05 14:15:38 +0800

    Clean up.

--- cmd/protoc-gen-go-svc-interface/main.go
+++ cmd/protoc-gen-go-svc-interface/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/types/pluginpb"
@@ -12,8 +11,6 @@ func main() {
 	plugin := &Plugin{}
 	protogen.Options{}.Run(func(gen *protogen.Plugin) error {
 		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
-
-		fmt.Fprintln(os.Stderr, gen.Request.GetParameter())
 		for _, f := range gen.Files {
 			if f.Generate {
 				plugin.GenerateServices(gen, f)
