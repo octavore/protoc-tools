8b6cb824188d1618a0e14184125209fb0585a014
tp <tp@octavore.com>
2024-02-05 14:11:31 +0800

    Return proto3 optional supported feature flag in go-svc-interface.

--- cmd/protoc-gen-go-svc-interface/main.go
+++ cmd/protoc-gen-go-svc-interface/main.go
@@ -5,16 +5,14 @@ import (
 	"os"
 
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
 	plugin := &Plugin{}
-	protogen.Options{
-		ParamFunc: func(name, value string) error {
-			fmt.Fprintln(os.Stderr, name, value)
-			return nil
-		},
-	}.Run(func(gen *protogen.Plugin) error {
+	protogen.Options{}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+
 		fmt.Fprintln(os.Stderr, gen.Request.GetParameter())
 		for _, f := range gen.Files {
 			if f.Generate {
